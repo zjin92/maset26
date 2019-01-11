6af6b7d9a90eb26508a80c8e136abcdda97e1153
autumnfish <517729329@qq.com>
2019-01-11 11:30:51 +0800

    增加登录跟详情组件

--- src/main.js
+++ src/main.js
@@ -9,21 +9,36 @@ import './assets/statics/site/css/style.css';
 
 Vue.config.productionTip = false
 
-
 // 导入额外的组件 路由使用的
 import index from './components/index.vue'
+import detail from './components/02.details.vue'
+import login from './components/03.login.vue'
 new Vue({
   render: h => h(App),
   // 挂载路由对象
-  router:new VueRouter({
-    routes:[
+  router: new VueRouter({
+    routes: [
+      // {
+      //   path:'/',
+      //   component:index
+      // },
+      // 使用重定向 把/ 定位到 /index
+      // 重定向居多 多个地址对应到同一个组件时
       {
-        path:'/',
-        component:index
+        path: '/',
+        redirect: '/index'
       },
       {
-        path:'/index',
-        component:index
+        path: '/index',
+        component: index
+      },
+      {
+        path: '/detail',
+        component: detail
+      },
+      {
+        path: '/login',
+        component: login
       }
     ]
   })
